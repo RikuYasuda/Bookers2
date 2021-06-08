8131770a3b1954057099ffa8808f6d0f9ced6ba0
RikuYasuda <antianl009@gmail.com>
2021-06-08 12:31:39 +0000

    [add]relationships

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get  "home/about"  => "homes#show"
   resources :users, only: [:index,:show, :edit,:update]
+  resource :relationships, only: [:create, :destroy]
+  get 'followings/:id' => 'relationships#followings', as: 'followings'
+  get 'followers/:id' => 'relationships#followers', as: 'followers'
 
   resources :books do
     resource :favorites, only: [:create,:destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_05_133634) do
+ActiveRecord::Schema.define(version: 2021_06_07_115924) do
 
   create_table "books", force: :cascade do |t|
     t.integer "user_id"
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 2021_06_05_133634) do
     t.string "image_id"
     t.text "caption"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
