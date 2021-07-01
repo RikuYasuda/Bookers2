ebe298ea1fd257e0805429cd584b1b1742899482
RikuYasuda <antianl009@gmail.com>
2021-07-01 01:56:57 +0000

    add7/1

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,15 @@
 class CommentsController < ApplicationController
 
   def create
+    @book = Book.find(params[:book_id])
     book = Book.find(params[:book_id])
     comment = current_user.comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
   end
 
   def destroy
     Comment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
   end
 
   private
